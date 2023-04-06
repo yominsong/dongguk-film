52fe0282b14fe5278522434e930a270cb4ba18f6
Yomin Song <alsthd27@naver.com>
2023-04-06 21:25:24 +0900

    Fix: Resolve an error that occurs when null is returned in JavaScript code related to navbar and footer

--- dongguk_film/static/js/navbar_footer.js
+++ dongguk_film/static/js/navbar_footer.js
@@ -1,10 +1,7 @@
 function hideNavbarAndFooter() {
+    let httpStatusCode = document.querySelector("#http_status_code");
     if (location.pathname.indexOf("accounts") != -1 ||
-        document.querySelector("#http_status_code").innerText == "400 Bad Request" ||
-        document.querySelector("#http_status_code").innerText == "404 Not Found" ||
-        document.querySelector("#http_status_code").innerText == "408 Request Timeout" ||
-        document.querySelector("#http_status_code").innerText == "500 Internal Server Error" ||
-        document.querySelector("#http_status_code").innerText == "502 Bad Gateway") {
+        httpStatusCode != null) {
         navbar.hidden = true;
         footer.hidden = true;
     };
