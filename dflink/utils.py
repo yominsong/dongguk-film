281206490cb4c42ee21827e7519fa2cefaa4d0ca
Yomin Song <alsthd27@naver.com>
2023-04-15 04:38:18 +0900

    Fix: Fix headers of DF Link requests

--- dflink/utils.py
+++ dflink/utils.py
@@ -46,8 +46,8 @@ def chap_gpt(prompt):
 
 def is_available(original_url):
     try:
-        # response = requests.get(original_url, headers=headers)
         with Session() as session:
+            session.mount("http://", HTTPAdapter(max_retries=3))
             session.mount("https://", HTTPAdapter(max_retries=3))
             response = session.get(original_url, headers=headers)
             result = True if response.status_code == 200 else False
