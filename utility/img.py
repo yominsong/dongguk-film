9affd76934d60b45035de169eab4427d71202134
Yomin Song <alsthd27@naver.com>
2023-07-01 04:36:22 +0900

    Test: Edit some codes in img.py

--- utility/img.py
+++ utility/img.py
@@ -22,22 +22,24 @@ def save_img(query: str, app_name: str):
 
     url = f"https://api.unsplash.com/photos/random?client_id={UNSPLASH_ACCESS_KEY}&count=5&orientation=landscape&query={query}"
     headers = {"accept": "application/json", "Accept-Version": "v1"}
-    response = requests.get(url, headers=headers).text
+    response = requests.get(url, headers=headers).json()
     image_list = []
     image_list_for_msg = []
 
-    for random_image in json.loads(response):
-        image_url = random_image[11][2]
-        image_list.append(image_url)
-        image = {"app_name": app_name, "image_url": image_url}
-        image_list_for_msg.append(image)
+    for random_image in response:
+        image_url = random_image
+        image_list_for_msg.append(image_url)
+    #     image_url = random_image["urls"]["regular"]
+    #     image_list.append(image_url)
+    #     image = {"app_name": app_name, "image_url": image_url}
+    #     image_list_for_msg.append(image)
 
-    with open(json_path, "r+") as f:
-        data = json.load(f)
-        data[app_name] = image_list
-        f.seek(0)
-        f.write(json.dumps(data, indent=4))
-        f.truncate()
+    # with open(json_path, "r+") as f:
+    #     data = json.load(f)
+    #     data[app_name] = image_list
+    #     f.seek(0)
+    #     f.write(json.dumps(data, indent=4))
+    #     f.truncate()
 
     return image_list_for_msg
 
