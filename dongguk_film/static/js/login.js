984102a6e4ab9377eea53b9d86bd2f643889ff43
Yomin Song <alsthd27@naver.com>
2023-07-02 18:30:47 +0900

    Feat: Add noti for blocking in-app browser

--- dongguk_film/static/js/login.js
+++ dongguk_film/static/js/login.js
@@ -3,22 +3,22 @@ function blockInAppBrowser() {
     let agent = navigator.userAgent.toLowerCase();
     let boolean, app;
 
-    login_google.addEventListener("mousedown", () => {
-        if ((agent.indexOf("kakaotalk")) !== -1) {
-            boolean, app = true, "카카오톡";
-        } else if ((agent.indexOf("naver")) !== -1) {
-            boolean, app = true, "네이버";
-        } else if ((agent.indexOf("fban")) !== -1 || (agent.indexOf("fbav")) !== -1) {
-            boolean, app = true, "페이스북";
-        } else if ((agent.indexOf("instagram")) !== -1) {
-            boolean, app = true, "인스타그램";
-        };
+    if ((agent.indexOf("kakaotalk")) !== -1) {
+        boolean, app = true, "카카오톡";
+    } else if ((agent.indexOf("naver")) !== -1) {
+        boolean, app = true, "네이버";
+    } else if ((agent.indexOf("fban")) !== -1 || (agent.indexOf("fbav")) !== -1) {
+        boolean, app = true, "페이스북";
+    } else if ((agent.indexOf("instagram")) !== -1) {
+        boolean, app = true, "인스타그램";
+    };
 
-        if (boolean == true) {
-            login_google.href = "#";
+    if (boolean == true) {
+        login_google.href = "#";
+        login_google.addEventListener("mousedown", () => {
             controlNoti("blockInAppBrowser", app);
-        };
-    });
+        });
+    };
 }
 
 blockInAppBrowser();
