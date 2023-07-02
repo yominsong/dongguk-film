13ef6ea1224fbe99bb998ca9b613fd369eca3db9
Yomin Song <alsthd27@naver.com>
2023-07-02 18:27:24 +0900

    Feat: Add noti for blocking in-app browser

--- dongguk_film/static/js/base-top.js
+++ dongguk_film/static/js/base-top.js
@@ -219,7 +219,7 @@ function controlNoti(notiType, params = null) {
         if (notiType == "blockInAppBrowser") {
             notiIconDefault.classList.remove("hidden");
             notiTitle.innerText = "'Google로 로그인'이 잠겼어요.";
-            notiContent.innerText = `${params} 인앱 브라우저에서는 'Google로 로그인'을 불가해요. Chrome이나 Safari에서는 가능해요!`;
+            notiContent.innerText = `${params} 인앱 브라우저에서는 'Google로 로그인'이 불가해요. Chrome이나 Safari에서는 가능해요!`;
         } else if (notiType == "welcomeNewUser") {
             notiIconSmile.classList.remove("hidden");
             notiTitle.innerText = "디닷에프 가입을 환영해요! 🎉";
--- dongguk_film/static/js/login.js
+++ dongguk_film/static/js/login.js
@@ -1,22 +1,23 @@
 function blockInAppBrowser() {
+    let login_google = document.querySelector("#login_google");
     let agent = navigator.userAgent.toLowerCase();
     let boolean, app;
-    if ((agent.indexOf("kakaotalk")) !== -1) {
-        boolean = true;
-        app = "카카오톡";
-    } else if ((agent.indexOf("naver")) !== -1) {
-        boolean = true;
-        app = "네이버";
-    } else if ((agent.indexOf("fban")) !== -1 || (agent.indexOf("fbav")) !== -1) {
-        boolean = true;
-        app = "페이스북";
-    } else if ((agent.indexOf("instagram")) !== -1) {
-        boolean = true;
-        app = "인스타그램";
-    };
-    if (boolean == true) {
-        controlNoti("blockInAppBrowser", app);
-    };
+
+    login_google.addEventListener("mousedown", () => {
+        if ((agent.indexOf("kakaotalk")) !== -1) {
+            boolean, app = true, "카카오톡";
+        } else if ((agent.indexOf("naver")) !== -1) {
+            boolean, app = true, "네이버";
+        } else if ((agent.indexOf("fban")) !== -1 || (agent.indexOf("fbav")) !== -1) {
+            boolean, app = true, "페이스북";
+        } else if ((agent.indexOf("instagram")) !== -1) {
+            boolean, app = true, "인스타그램";
+        };
+
+        if (boolean == true) {
+            controlNoti("blockInAppBrowser", app);
+        };
+    });
 }
 
 blockInAppBrowser();
