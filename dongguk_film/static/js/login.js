d4bbea2db19ef668817231327b8619384048716d
Yomin Song <alsthd27@naver.com>
2023-07-02 18:51:38 +0900

    Feat: Add noti for blocking in-app browser

--- dongguk_film/static/js/login.js
+++ dongguk_film/static/js/login.js
@@ -4,13 +4,13 @@ function blockInAppBrowser() {
     let boolean, app;
 
     if ((agent.indexOf("kakaotalk")) !== -1) {
-        boolean, app = true, "카카오톡";
+        [boolean, app] = [true, "카카오톡"];
     } else if ((agent.indexOf("naver")) !== -1) {
-        boolean, app = true, "네이버";
+        [boolean, app] = [true, "네이버"];
     } else if ((agent.indexOf("fban")) !== -1 || (agent.indexOf("fbav")) !== -1) {
-        boolean, app = true, "페이스북";
+        [boolean, app] = [true, "페이스북"];
     } else if ((agent.indexOf("instagram")) !== -1) {
-        boolean, app = true, "인스타그램";
+        [boolean, app] = [true, "인스타그램"];
     };
 
     if (boolean == true) {
