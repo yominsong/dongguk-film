937591fd1e0b1a0c8b06a1a30cfd36a0bccd0fc0
Yomin Song <alsthd27@naver.com>
2023-07-16 01:37:54 +0900

    Chore: Update requirements.txt and change datetime module to Python standard library rather than Django's library

--- cronjobs.py
+++ cronjobs.py
@@ -1,16 +1,9 @@
-from django.utils import timezone
-from requests.sessions import Session
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-import requests
+from datetime import datetime
+import pytz, requests
 
 """
 If the time is not specified with an if statement, it is executed every 30 minutes.
 """
-
-# session = Session()
-# retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
-# session.mount("https://", HTTPAdapter(max_retries=retries))
 
 #
 # users
@@ -18,22 +11,22 @@ If the time is not specified with an if statement, it is executed every 30 minut
 
 requests.get("https://dongguk.film/users/utils/delete-expired-vcodes")
 
-if timezone.now().strftime("%H:%M") == "08:00":
+if datetime.now(pytz.timezone("Asia/Seoul")).strftime("%H:%M") == "08:00":
     requests.get("https://dongguk.film/users/utils/delete-inactive-users")
 
 #
 # dflink
 #
 
-if timezone.now().strftime("%H:%M") == "00:00":
+if datetime.now(pytz.timezone("Asia/Seoul")).strftime("%H:%M") == "00:00":
     requests.get("https://dongguk.film/dflink/utils/delete-expired-dflinks")
 
 #
 # utility
 #
 
-if timezone.now().strftime("%H:%M") == "23:00":
+if datetime.now(pytz.timezone("Asia/Seoul")).strftime("%H:%M") == "23:00":
     requests.get("https://dongguk.film/utility/utils/update-img")
 
-if timezone.now().strftime("%H:%M") == "23:30":
+if datetime.now(pytz.timezone("Asia/Seoul")).strftime("%H:%M") == "23:30":
     requests.get("https://dongguk.film/utility/utils/update-dmd-cookie")
