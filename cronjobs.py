ce71ca5015f5370bdc9687d43e0de3f164b0ff8d
Yomin Song <alsthd27@naver.com>
2023-07-16 00:43:39 +0900

    Test: Test cronjobs.py

--- cronjobs.py
+++ cronjobs.py
@@ -1,20 +1,19 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dongguk_film.settings")
+django.setup()
+
 from django.utils import timezone
 
-from requests.sessions import Session
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
+# from requests.sessions import Session
+# from requests.adapters import HTTPAdapter
+# from urllib3.util.retry import Retry
 
 from django.core.wsgi import get_wsgi_application
 from django.core.handlers.wsgi import WSGIRequest
 from dflink.utils import delete_expired_dflinks
 
-import os
-
-from django.conf import settings
-
-settings.configure()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dongguk_film.settings")
 application = get_wsgi_application()
 
 request = WSGIRequest(
@@ -28,33 +27,33 @@ request = WSGIRequest(
 If the time is not specified with an if statement, it is executed every 30 minutes.
 """
 
-session = Session()
-retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
-session.mount("https://", HTTPAdapter(max_retries=retries))
+# session = Session()
+# retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+# session.mount("https://", HTTPAdapter(max_retries=retries))
 
 #
 # users
 #
 
-session.get("https://dongguk.film/users/utils/delete-expired-vcodes")
+# session.get("https://dongguk.film/users/utils/delete-expired-vcodes")
 delete_expired_dflinks(request)
 
-if timezone.now().strftime("%H:%M") == "08:00":
-    session.get("https://dongguk.film/users/utils/delete-inactive-users")
+# if timezone.now().strftime("%H:%M") == "08:00":
+#     session.get("https://dongguk.film/users/utils/delete-inactive-users")
 
 #
 # dflink
 #
 
-if timezone.now().strftime("%H:%M") == "00:00":
-    session.get("https://dongguk.film/dflink/utils/delete-expired-dflinks")
+# if timezone.now().strftime("%H:%M") == "00:00":
+#     session.get("https://dongguk.film/dflink/utils/delete-expired-dflinks")
 
 #
 # utility
 #
 
-if timezone.now().strftime("%H:%M") == "23:00":
-    session.get("https://dongguk.film/utility/utils/update-img")
+# if timezone.now().strftime("%H:%M") == "23:00":
+#     session.get("https://dongguk.film/utility/utils/update-img")
 
-if timezone.now().strftime("%H:%M") == "23:30":
-    session.get("https://dongguk.film/utility/utils/update-dmd-cookie")
+# if timezone.now().strftime("%H:%M") == "23:30":
+#     session.get("https://dongguk.film/utility/utils/update-dmd-cookie")
