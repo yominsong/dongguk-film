62ef27b9c75fe6093caedda747268cafd7d987cf
Yomin Song <alsthd27@naver.com>
2023-07-15 19:02:14 +0900

    Test: Edit cronjobs.py

--- cronjobs.py
+++ cronjobs.py
@@ -1,10 +1,24 @@
 from django.utils import timezone
+
 from requests.sessions import Session
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+
+from django.core.wsgi import get_wsgi_application
+from django.core.handlers.wsgi import WSGIRequest
+from dflink.utils import delete_expired_dflinks
+
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dongguk_film.settings")
+application = get_wsgi_application()
+
+request = WSGIRequest(
+    {
+        "REQUEST_METHOD": "GET",
+        "PATH_INFO": "cronjobs.py",
+    }
+)
 
 """
 If the time is not specified with an if statement, it is executed every 30 minutes.
@@ -19,6 +33,7 @@ session.mount("https://", HTTPAdapter(max_retries=retries))
 #
 
 session.get("https://dongguk.film/users/utils/delete-expired-vcodes")
+delete_expired_dflinks(request)
 
 if timezone.now().strftime("%H:%M") == "08:00":
     session.get("https://dongguk.film/users/utils/delete-inactive-users")
