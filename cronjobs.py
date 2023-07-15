71391eccf639f3c3f5fab82b2d555e7027b79e25
Yomin Song <alsthd27@naver.com>
2023-07-15 17:09:00 +0900

    Fix: Add code to retry cronjobs on error

--- cronjobs.py
+++ cronjobs.py
@@ -1,4 +1,6 @@
 from django.utils import timezone
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 import os, requests
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dongguk_film.settings")
@@ -7,28 +9,34 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dongguk_film.settings")
 If the time is not specified with an if statement, it is executed every 30 minutes.
 """
 
+session = requests.Session()
+retries = Retry(total=5,
+                backoff_factor=0.1,
+                status_forcelist=[ 500, 502, 503, 504 ])
+session.mount('https://', HTTPAdapter(max_retries=retries))
+
 #
 # users
 #
 
-requests.get("https://dongguk.film/users/utils/delete-expired-vcodes")
+session.get("https://dongguk.film/users/utils/delete-expired-vcodes")
 
 if timezone.now().strftime("%H:%M") == "08:00":
-    requests.get("https://dongguk.film/users/utils/delete-inactive-users")
+    session.get("https://dongguk.film/users/utils/delete-inactive-users")
 
 #
 # dflink
 #
 
 if timezone.now().strftime("%H:%M") == "00:00":
-    requests.get("https://dongguk.film/dflink/utils/delete-expired-dflinks")
+    session.get("https://dongguk.film/dflink/utils/delete-expired-dflinks")
 
 #
 # utility
 #
 
 if timezone.now().strftime("%H:%M") == "23:00":
-    requests.get("https://dongguk.film/utility/utils/update-img")
+    session.get("https://dongguk.film/utility/utils/update-img")
 
 if timezone.now().strftime("%H:%M") == "23:30":
-    requests.get("https://dongguk.film/utility/utils/update-dmd-cookie")
+    session.get("https://dongguk.film/utility/utils/update-dmd-cookie")
