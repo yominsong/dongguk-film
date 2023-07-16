b1f8b980f142d2f3b4b7895c34f966867a4b8c63
Yomin Song <alsthd27@naver.com>
2023-07-16 21:20:15 +0900

    Fix: Apply Django template tag's value into

--- dongguk_film/static/js/push.js
+++ dongguk_film/static/js/push.js
@@ -14,11 +14,12 @@ function requestConsentForPushNoti() {
 function controlPush() {
     if ("Notification" in window) {
         let permission = Notification.permission;
+        let student_id = document.getElementById("push").dataset.studentId;
 
         if (permission == "granted") {
             FlareLane.initialize({ projectId: "f7935dfb-3492-4cee-9ddc-9eb74d30602e" });
             FlareLane.setIsSubscribed(true);
-            FlareLane.setUserId("{{ request.user.metadata.student_id }}");
+            FlareLane.setUserId(student_id);
         } else if (permission == "denied") {
             requestConsentForPushNoti();
         };
