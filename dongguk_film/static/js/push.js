8da62a87abc4f7849c7b74468b0010f41df680ea
Yomin Song <alsthd27@naver.com>
2023-07-16 21:36:53 +0900

    Feat: Add user's OS name into FlareLane UserID param

--- dongguk_film/static/js/push.js
+++ dongguk_film/static/js/push.js
@@ -6,6 +6,22 @@ function requestConsentForPushNoti() {
     controlNoti("requestPushPermission");
 }
 
+function getOS() {
+    let userAgent = navigator.userAgent || window.opera;
+    let result = "Unknown OS";
+
+    // Mobile
+    if (/windows phone/i.test(userAgent)) { result = "Windows Mobile" };
+    if (/android/i.test(userAgent)) { result = "Android" };
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) { result = "iOS" };
+
+    // Desktop
+    if (/Win/.test(userAgent)) { result = "Windows" };
+    if (/Mac/i.test(userAgent)) { result = "MacOS" };
+    if (/Linux/.test(userAgent)) { result = "Linux" };
+
+    return result;
+}
 
 //
 // Main functions
@@ -14,12 +30,12 @@ function requestConsentForPushNoti() {
 function controlPush() {
     if ("Notification" in window) {
         let permission = Notification.permission;
-        let student_id = document.getElementById("push").dataset.studentId;
+        let student_id = document.getElementById("pushJS").dataset.studentId;
 
         if (permission == "granted") {
             FlareLane.initialize({ projectId: "f7935dfb-3492-4cee-9ddc-9eb74d30602e" });
             FlareLane.setIsSubscribed(true);
-            FlareLane.setUserId(student_id);
+            FlareLane.setUserId(`${student_id} (${getOS()})`);
         } else if (permission == "denied") {
             requestConsentForPushNoti();
         };
