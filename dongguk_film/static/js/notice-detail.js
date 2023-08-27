d63159405be735dedd4d8fb7c81c935bdaf790b8
Yomin Song <alsthd27@naver.com>
2023-08-27 17:09:03 +0900

    Fix: Fix a bug that prevents YouTube contents from displaying on Notice detail page

--- dongguk_film/static/js/notice-detail.js
+++ dongguk_film/static/js/notice-detail.js
@@ -8,6 +8,30 @@ const id_go_to_list = document.getElementById("id_go_to_list");
 // Sub functions
 //
 
+function showYouTube() {
+    let mediaElements = document.querySelectorAll("figure.media");
+
+    if (mediaElements) {
+        mediaElements.forEach(mediaElement => {
+            let oembedElement = mediaElement.querySelector("oembed");
+            let url = oembedElement.getAttribute("url");
+            let videoId = url.split("v=")[1];
+            let newStructure = `
+            <figure class="media ck-widget" contenteditable="false">
+                <div class="ck-media__wrapper" data-oembed-url="${url}">
+                    <div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">
+                        <iframe src="https://www.youtube.com/embed/${videoId}" style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+                    </div>
+                </div>
+            </figure>`;
+    
+            mediaElement.outerHTML = newStructure;
+        });
+    };
+}
+
+showYouTube();
+
 function goToList() {
     ["click", "keyup"].forEach(type => {
         id_go_to_list.addEventListener(type, (event) => {
