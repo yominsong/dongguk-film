c1ecd731c924c4d1fd73ce6638c959e6653e1410
Yomin Song <ssongyo@gmail.com>
2024-04-20 16:44:15 +0900

    Chore: Change the scrollTo() value for scrolling search filters in the Equipment app

--- dongguk_film/static/js/equipment.js
+++ dongguk_film/static/js/equipment.js
@@ -153,8 +153,8 @@ function executeWhenPurposeIsSelected(selectedPurpose = null) {
         data_period.startDateMin = formatDateInFewDays(now, data_purpose.atLeast);
         data_period.startDateMax = formatDateInFewDays(now, data_purpose.upTo);
         id_period_calendar.classList.replace("bg-gray-100", "bg-white");
-        id_scrollable_part_of_modal.scrollTo({ top: id_scrollable_part_of_modal.scrollHeight, behavior: "smooth" });
-        setTimeout(() => { yAxisScrollPositionValueForModal = id_scrollable_part_of_modal.scrollTop }, 500); // When the behavior option of the scrollTo() method is set to smooth, the duration varies across web browsers, so a random estimate of 300ms is applied to setTimeout()
+        id_scrollable_part_of_modal.scrollTo({ top: id_scrollable_part_of_modal.scrollHeight + id_period_help.offsetHeight, behavior: "smooth" });
+        setTimeout(() => { yAxisScrollPositionValueForModal = id_scrollable_part_of_modal.scrollTop }, 500); // When the behavior option of the scrollTo() method is set to smooth, the duration varies across web browsers, so a random estimate of 500ms is applied to setTimeout()
     } else {
         id_purpose.value = null;
         id_initialize_purpose.hidden = true;
