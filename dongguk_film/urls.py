1ec93f183edfb717eafc51897c4663417e55df54
Yomin Song <200yomin@gmail.com>
2024-05-07 19:09:11 +0900

    Chore: Add robots.txt

--- dongguk_film/urls.py
+++ dongguk_film/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -30,11 +31,19 @@ urlpatterns = [
     path("notice/", include("notice.urls")),
     path("users/", include("users.urls")),
     path("utility/", include("utility.urls")),
-    re_path(
-        r"^sw.js$",
+    path(
+        "robots.txt",
         serve_static,
-        {"document_root": settings.STATIC_ROOT, "path": "js/sw.js"},
+        {
+            "path": "robots.txt",
+            "document_root": settings.STATIC_ROOT,
+        },
     ),
+    # re_path(
+    #     r"^sw.js$",
+    #     serve_static,
+    #     {"document_root": settings.STATIC_ROOT, "path": "js/sw.js"},
+    # ),
     # re_path(
     #     r"^sw.js$",
     #     serve_static,
