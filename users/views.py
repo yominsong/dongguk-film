6ccffad4ee8e425ad7f78ac615579d3b28f02ff1
Yomin Song <200yomin@gmail.com>
2024-08-06 22:37:49 +0900

    Feat: Add 'My information' to the Users app Account page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from utility.img import get_hero_img
+from utility.utils import format_datetime
 
 #
 # Main functions
@@ -10,5 +11,10 @@ from utility.img import get_hero_img
 @login_required
 def account(request):
     image_list = get_hero_img("account")
+    date_joined = format_datetime(request.user.date_joined)
 
-    return render(request, "users/account.html", {"image_list": image_list})
+    return render(
+        request,
+        "users/account.html",
+        {"image_list": image_list, "date_joined": date_joined},
+    )
